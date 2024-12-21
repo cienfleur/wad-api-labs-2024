4b4b8332b08dbfb773b43907a76e7af65f1da1a3
Nari <redvelvet0214@protonmail.ch>
2024-12-21 19:40:32 +0000

    Salt and hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,30 @@ const pwdValidator = (password) => {
     return password = regex.test(password);
   }
   UserSchema.path("password").validate(pwdValidator);
+
+  UserSchema.methods.comparePassword = async function (passw) { 
+    return await bcrypt.compare(passw, this.password); 
+  }
+
+  UserSchema.statics.findByUserName = function (username) {
+    return this.findOne({ username: username });
+  };
+
+  UserSchema.pre('save', async function(next) {
+    const saltRounds = 10; // You can adjust the number of salt rounds
+    //const user = this;
+    if (this.isModified('password') || this.isNew) {
+      try {
+        const hash = await bcrypt.hash(this.password, saltRounds);
+        this.password = hash;
+        next();
+    } catch (error) {
+       next(error);
+    }
+  
+    } else {
+        next();
+    }
+  });
 
 export default mongoose.model('User', UserSchema);
